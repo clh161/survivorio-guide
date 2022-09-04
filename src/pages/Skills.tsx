12d9f92ed6539056af6acc6a9e1afaddf5384e33
Jacob Ho <clh161@gmail.com>
2022-09-04 01:11:32 +0100

    dep

---
+++ src/pages/Skills.tsx
@@ -0,0 +1,31 @@
+import { Paper, TableHead, TableBody } from '@mui/material';
+import Table from '@mui/material/Table/Table';
+import TableCell from '@mui/material/TableCell/TableCell';
+import TableContainer from '@mui/material/TableContainer/TableContainer';
+import TableRow from '@mui/material/TableRow/TableRow';
+import React from 'react';
+
+export default function Skills() {
+  const skills = [
+    {
+      name: 'Kunai',
+    },
+  ];
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="Skill table">
+        <TableHead>
+          <TableRow>{/* <TableCell>Name</TableCell> */}</TableRow>
+        </TableHead>
+        <TableBody>
+          {skills.map((skill) => (
+            <TableRow key={skill.name}>
+              <TableCell>{skill.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
